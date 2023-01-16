de00a4da25cc904d90e2d0acef004f64220c6985
Zubair Ahmed <zubair1024@gmail.com>
2023-01-17 03:30:00 +0400

    adding services section images

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
 
 interface IServiceCardProps {
   title: string;
@@ -12,13 +11,13 @@ const serviceItems: IServiceCardProps[] = [
     title: "Weatherproofing - EIF",
     description:
       "Lorem ipsum dolor sit amet. Et reiciendis alias sit nesciunt illum qui vero error in optio rerum ut doloremque ducimus. Est aperiam itaque qui asperiores praesentium eum alias possimus ut facilis itaque qui sunt placeat. Sit galisum omnis et dolorum ipsam sit repellat quibusdam.",
-    image: "/services/large-format-tiles.jpg",
+    image: "/services/eifs.jpg",
   },
   {
     title: "Cement Composition Cladding",
     description:
       "Lorem ipsum dolor sit amet. Et reiciendis alias sit nesciunt illum qui vero error in optio rerum ut doloremque ducimus. Est aperiam itaque qui asperiores praesentium eum alias possimus ut facilis itaque qui sunt placeat. Sit galisum omnis et dolorum ipsam sit repellat quibusdam.",
-    image: "/services/large-format-tiles.jpg",
+    image: "/services/cement-composition.jpg",
   },
   {
     title: "Porcelain Cladding Systems",
@@ -30,32 +29,36 @@ const serviceItems: IServiceCardProps[] = [
     title: "Large Format Tiles",
     description:
       "Lorem ipsum dolor sit amet. Et reiciendis alias sit nesciunt illum qui vero error in optio rerum ut doloremque ducimus. Est aperiam itaque qui asperiores praesentium eum alias possimus ut facilis itaque qui sunt placeat. Sit galisum omnis et dolorum ipsam sit repellat quibusdam.",
-    image: "/services/large-format-tiles.jpg",
+    image: "/services/large-format-tiles-2.jpg",
   },
 ];
 
 const ServiceCard = (props: IServiceCardProps) => {
   return (
-    <motion.div
-      className="flex space-x-6 md:space-x-0 md:flex-col justify-center items-center p-5 bg-[#fff] shadow-2xl rounded-xl md:max-w-xs mt-3 md:mx-2"
-      initial={{ opacity: 0, y: 300 }}
-      animate={{ opacity: 1, y: 0 }}
-    >
-      <div className="">
-        <Image
-          alt={props.title}
-          src={props.image}
-          height={150}
-          width={150}
-        ></Image>
-      </div>
-      <div>
-        <h2 className="py-2 text-xl md:text-center ">{props.title}</h2>
-        <p className="text-sm font-light tracking-tighter">
-          {props.description}
-        </p>
-      </div>
-    </motion.div>
+    <div className="transition ease-in hover:scale-105">
+      <motion.div
+        className="flex flex-col space-x-6 md:space-x-0 md:flex-col justify-center items-center bg-[#fff] shadow-2xl rounded-xl md:max-w-xs mt-3 md:mx-2"
+        initial={{ opacity: 0, y: 300 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <div
+          className=" flex flex-col justify-end w-full h-[300px] text-[#FFF] rounded-xl"
+          style={{
+            backgroundImage: `url(${props.image})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <div className=" min-h-[80px] flex flex-col justify-end w-full bg-gradient-to-t from-[#000] to-[transparent]">
+            <h2 className="py-2 text-xl text-center font-lato">
+              {props.title}
+            </h2>
+          </div>
+        </div>
+        <div className="p-5">
+          <p className="text-sm font-light ">{props.description}</p>
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
